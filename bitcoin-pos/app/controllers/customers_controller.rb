4ad580c1f303a41ceef6c76e67695760e6fbb3f5
Michaeltym <yimingt1@student.unimelb.edu.au>
2014-12-05 13:21:01 +1100

    invoke wallet api

--- bitcoin-pos/app/controllers/customers_controller.rb
+++ bitcoin-pos/app/controllers/customers_controller.rb
@@ -2,10 +2,16 @@
 class CustomersController < ApplicationController
   def index
 	money=(params[:beer].to_i*0.0001.to_f+params[:water].to_i*0.0002.to_f+params[:champange].to_i*0.0003.to_f).to_s 
-	url='bitcoin:15RPiw5v2ses7o3xqbQe6PAvqxKHHeomG1?amount='+money
+	url='bitcoin:1E3z7bn4ZeBa47GBBBSfWBFyw1TrXYonxS?amount='+money
 	@qr = RQRCode::QRCode.new(url, :size => 4, :level => 'l'.to_sym)
-	@wallet = Blockchain::Wallet.new("4c95617c-6274-4732-acdf-6ecc2b03fc28", "tanyiming911114", nil)
-	@address=@wallet.addresses
-	#@address=@address1[balance]
+	personal_wallet = Blockchain::Wallet.new("4c95617c-6274-4732-acdf-6ecc2b03fc28", "tanyiming911114", nil)
+	@wallet=personal_wallet.addresses
+	#@test=@wallet[0]['address']
+
+	account=personal_wallet.address_balance("1E3z7bn4ZeBa47GBBBSfWBFyw1TrXYonxS")
+	@balance=account.balance
+	@receive=account.total_received
+	@address=account.address
+	#@ticker = Blockchain::get_ticker()
   end
 end
