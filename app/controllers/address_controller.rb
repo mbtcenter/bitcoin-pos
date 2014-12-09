5b80809069f6aa40b6bd5359bb0e0e501b6d402c
WXB <xibowangmelb@gmail.com>
2014-12-09 12:20:30 +1100

    hehehe

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -3,7 +3,9 @@ class AddressController < ApplicationController
   end
 
   def index
-  	personal_wallet = Blockchain::Wallet.new("4c95617c-6274-4732-acdf-6ecc2b03fc28","tanyiming911114",nil)
+  	user = ENV['blockchain_userid']
+    pass = ENV['blockchain_userpass']
+  	personal_wallet = Blockchain::Wallet.new(user,pass,nil)
     @addresses = personal_wallet.addresses
   end
 end
