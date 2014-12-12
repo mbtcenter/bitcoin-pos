efa4f6aafa48c85183cfcc0bf85442745f921005
Michaeltym <1412909864@qq.com>
2014-12-12 12:23:56 +1100

    test file

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -3,10 +3,10 @@ class AddressController < ApplicationController
   end
 
   def index
-  	user='4c95617c-6274-4732-acdf-6ecc2b03fc28'
-  	pass='tanyiming911114'
-  	#user = ENV['blockchain_userid']
-    #pass = ENV['blockchain_userpass']
+  	#user='4c95617c-6274-4732-acdf-6ecc2b03fc28'
+  	#pass='tanyiming911114'
+  	user = ENV['blockchain_userid']
+    pass = ENV['blockchain_userpass']
   	personal_wallet = Blockchain::Wallet.new(user,pass,nil)
     @addresses = personal_wallet.addresses
   end
