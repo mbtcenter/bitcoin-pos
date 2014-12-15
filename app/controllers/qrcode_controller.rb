bd0239929469ed9cfea85ca1bf3d79490697d6f4
WXB <xibowangmelb@gmail.com>
2014-12-15 12:08:13 +1100

    params pass issuse resolved

--- app/controllers/qrcode_controller.rb
+++ app/controllers/qrcode_controller.rb
@@ -3,7 +3,7 @@ class QrcodeController < ApplicationController
   def index
   	@address=params[:address]
   	@amount=params[:display]
-    url='bitcoin:'+ "1" +'?amount=' + "1"
+    url='bitcoin:'+ @address +'?amount=' + @amount
     @qr = RQRCode::QRCode.new(url, :size => 4, :level => 'l'.to_sym)  
   end
   def show
