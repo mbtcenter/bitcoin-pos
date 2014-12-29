9b45c17867a34d6688c2b57633a6b63a6ebf78fb
Michaeltym <1412909864@qq.com>
2014-12-29 17:18:50 +1100

    delete

--- bitcoin-pos/app/controllers/calculator_controller.rb
+++
@@ -1,6 +0,0 @@
-class CalculatorController < ApplicationController
-  def index
-  	@address = params[:address]
-  	@balance = (params[:balance].to_f/(10**8)).to_f
-  end
-end
--- bitcoin-pos/test/helpers/calculator_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CalculatorHelperTest < ActionView::TestCase
-end
